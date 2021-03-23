e081c01f191fcb30be3088c2e63ea74590b764ed
Sancheet <sanchethoque@gmail.com>
2021-03-23 16:45:39 -0400

    Adding preliminary pokemon generation code

---
+++ PokeGen/index.js
@@ -0,0 +1,41 @@
+const natures = require("./natures");
+const cities = require("./cities");
+const pokedex = require("./pokedex");
+const Pokemon = require("../models/pokemon");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/pokebank", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection Open");
+  })
+  .catch((err) => {
+    console.log("Error!!");
+    console.log(err);
+  });
+
+const randEl = (array) => array[Math.floor(Math.random() * array.length)];
+
+const pokeGen = async () => {
+  await Pokemon.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const randomPoke = randEl(pokedex);
+    const randomNature = randEl(natures);
+    const randomCity = randEl(cities);
+    const poke = new Pokemon({
+      Location: `${randomCity.city}, ${randomCity.state}`,
+      Name: `${randomNature} ${randomPoke.name.english}`,
+      Id: `${randomPoke.id}`,
+      Level: `${Math.floor(Math.random() * 100) + 1}`,
+      Sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/${randomPoke.id}.png`,
+    });
+    await poke.save();
+  }
+};
+pokeGen().then(() => {
+  mongoose.connection.closed();
+});
