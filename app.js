36ab52b62e4a69e9f07569431e8c97ecce31d2e7
Sancheet <sanchethoque@gmail.com>
2021-03-24 19:09:39 -0400

    Added and updated the express routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
+
 const Pokemon = require("./models/pokemon");
 const natures = require("./PokeGen/natures");
 const pokedex = require("./PokeGen/pokedex");
@@ -25,6 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -40,12 +43,37 @@ app.get("/pokemon/new", (req, res) => {
 });
 
 app.post("/pokemon", async (req, res) => {
-  res.send(req.body);
+  req.body.pokemon.Name = pokedex[req.body.pokemon.DexNum - 1].name.english;
+  const newPoke = new Pokemon(req.body.pokemon);
+  await newPoke.save();
+  res.redirect(`pokemon/${newPoke._id}`);
 });
 
 app.get("/pokemon/:id", async (req, res) => {
   const pokemon = await Pokemon.findById(req.params.id);
   res.render("wildPokemon/show", { pokemon });
+});
+
+app.get("/pokemon/:id/edit", async (req, res) => {
+  const oldPoke = await Pokemon.findById(req.params.id);
+  res.render("wildPokemon/edit", { oldPoke, natures, pokedex });
+});
+
+app.put("/pokemon/:id", async (req, res) => {
+  req.body.pokemon.Name = pokedex[req.body.pokemon.DexNum - 1].name.english;
+  const { id } = req.params;
+  console.log(req.body.pokemon.DexNum);
+  const updatePoke = await Pokemon.findByIdAndUpdate(id, {
+    ...req.body.pokemon,
+  });
+  res.redirect(`/pokemon/${updatePoke._id}`);
+});
+
+app.delete("/pokemon/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Pokemon.findByIdAndDelete(id);
+  res.redirect("/pokemon");
 });
 
 app.listen(3000, () => {
